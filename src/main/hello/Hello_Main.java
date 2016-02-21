ccc594c528d8be797c13c37005e838320d346d36
jagdeep <jsingh312@gmail.com>
2016-02-22 01:39:35 +0530

    added another comment

--- src/main/hello/Hello_Main.java
+++ src/main/hello/Hello_Main.java
@@ -14,7 +14,7 @@ public class Hello_Main {
 	 */
 	public static void main(String[] args) {
 		System.out.println("Hello World Experim");
-
+		System.out.println("it not runs on ubuntu eclipse neon");
 	}
 
 }
